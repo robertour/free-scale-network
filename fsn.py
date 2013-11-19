257d4987a451d6b5f9eb2cb4ae9ab4e99cb54c45
Gileno Filho <gascf.cin@gmail.com>
2013-11-19 15:15:18 -0300

    ajustando exibição do gráfico usando networkx

--- fsn.py
+++ fsn.py
@@ -1,6 +1,9 @@
 import random
 import csv
 
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def weighted_random(weights):
     number = random.random() * sum(weights.values())
     for k,v in weights.items():
@@ -15,18 +18,19 @@ class FreeScaleNetwork(object):
     nodes = 0
     edges = []
 
-    def add_node(self):
-        current_node = self.nodes + 1
-        if self.nodes == 1:
-            self.edges.append((current_node, 1))
-        elif self.nodes > 1:
-            probabilities = self.calculate_probability()
-            first_node = weighted_random(probabilities)
-            del probabilities[first_node]
-            second_node = weighted_random(probabilities)
-            self.edges.append((current_node, first_node))
-            self.edges.append((current_node, second_node))
-        self.nodes = current_node
+    def add_node(self, nodes=1):
+        for i in range(nodes):
+            current_node = self.nodes + 1
+            if self.nodes == 1:
+                self.edges.append((current_node, 1))
+            elif self.nodes > 1:
+                probabilities = self.calculate_probability()
+                first_node = weighted_random(probabilities)
+                del probabilities[first_node]
+                second_node = weighted_random(probabilities)
+                self.edges.append((current_node, first_node))
+                self.edges.append((current_node, second_node))
+            self.nodes = current_node
 
     def calculate_probability(self):
         degrees = {}
@@ -56,6 +60,37 @@ class FreeScaleNetwork(object):
             writer.writerow(["Source", "Targe"])
             for edge in self.edges:
                 writer.writerow([edge[0], edge[1]])
+
+    def add_nodes_and_edges(self, graph):
+        for node in range(self.nodes):
+            graph.add_node(node)
+
+        for edge in self.edges:
+            graph.add_edge(edge[0], edge[1])
+
+    def draw_centralized(self):
+        graph = nx.Graph()
+
+        self.add_nodes_and_edges(graph)
+
+        pos = nx.shell_layout(graph)
+        nx.draw(graph, pos)
+
+        plt.show()
+
+    def draw_random(self):
+        graph = nx.random_geometric_graph(200,0.125)
+
+        self.add_nodes_and_edges(graph)
+
+        pos = nx.get_node_attributes(graph, 'pos')
+
+        nx.draw(graph, pos)
+
+        plt.xlim(-0.05,1.05)
+        plt.ylim(-0.05,1.05)
+        plt.axis('off')
+        plt.show()
 
     def __str__(self):
         return 'Nodes: {0}\nEdges: {1}'.format(self.nodes, self.edges)
